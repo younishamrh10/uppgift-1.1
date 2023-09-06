67b4418b80a46993869865d7f100857bd7689da8
yoha06001 <yoha06001@E-PW0234KT>
2023-09-06 10:25:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); // Jag skrev texten
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\"); // jag skrev texten med "
+        }
+    }
+}
